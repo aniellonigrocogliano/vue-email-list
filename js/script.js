f5f02ae45690f9e65bd48be7103cc0fdc13cdfb7
aniellonigrocogliano <aniello.nigro.cogliano@gmail.com>
2024-04-29 17:07:17 +0200

    add: style e bottone per generare email

--- js/script.js
+++ js/script.js
@@ -5,29 +5,23 @@ createApp({
         return {
             arrayEmail: [],
             element:"",
-            flag:false,
+            flag:0,
         };
     },
     created() {
-       this.generateArray();
     },
     methods: {
         generateArray: function () {
-
+            this.flag=2;
             for (let i = 0; i < 10; i++) {
-                
                 axios
                     .get("https://flynn.boolean.careers/exercises/api/random/mail")
                     .then((resp) => {
                         console.log(resp);
                         this.arrayEmail[i] = resp.data.response;
-                        if(i===9){this.flag=true;}
-                    });
-                    
-                    
-            }
-            
-        },
-        
+                        if(i===9){this.flag=1;}
+                    });  
+            } 
+        },  
     },
 }).mount("#app");
