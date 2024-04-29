4d0ba652e04c720529993269016fd91ef9e02c38
aniellonigrocogliano <aniello.nigro.cogliano@gmail.com>
2024-04-29 16:50:56 +0200

    add: scritta di caricamento

--- js/script.js
+++ js/script.js
@@ -13,17 +13,20 @@ createApp({
     },
     methods: {
         generateArray: function () {
-            for (let i = 0; i < 10; i++) {
 
+            for (let i = 0; i < 10; i++) {
+                
                 axios
                     .get("https://flynn.boolean.careers/exercises/api/random/mail")
                     .then((resp) => {
                         console.log(resp);
                         this.arrayEmail[i] = resp.data.response;
+                        if(i===9){this.flag=true;}
                     });
                     
+                    
             }
-            this.flag=true;
+            
         },
         
     },
