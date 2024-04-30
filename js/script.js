cd2292674655fdc2a2f12796facff1fd17bedb72
aniellonigrocogliano <aniello.nigro.cogliano@gmail.com>
2024-04-30 09:46:50 +0200

    fix: eliminazione variabile non utilizzata

--- js/script.js
+++ js/script.js
@@ -4,24 +4,23 @@ createApp({
     data() {
         return {
             arrayEmail: [],
-            element:"",
-            flag:0,
+            flag: 0,
         };
     },
     created() {
     },
     methods: {
         generateArray: function () {
-            this.flag=2;
+            this.flag = 2;
             for (let i = 0; i < 10; i++) {
                 axios
                     .get("https://flynn.boolean.careers/exercises/api/random/mail")
                     .then((resp) => {
                         console.log(resp);
                         this.arrayEmail[i] = resp.data.response;
-                        if(i===9){this.flag=1;}
-                    });  
-            } 
-        },  
+                        if (i === 9) { this.flag = 1; }
+                    });
+            }
+        },
     },
 }).mount("#app");
